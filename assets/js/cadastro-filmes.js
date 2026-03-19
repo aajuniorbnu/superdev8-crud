4a30fcaec8a3616a58ac655ce1ba9da9612ff494
aajuniorbnu <aajuniorbnu@gmail.com>
2026-03-19 11:15:00 -0300

    filmes ...falta acabar JS

---
+++ assets/js/cadastro-filmes.js
@@ -0,0 +1,16 @@
+let filmes = [];
+let contador = 1;
+
+function salvar(){
+const codigo = document.getElementById('codigo').value;
+
+const nome = document.getElementById('nome').value;
+const genero = document.getElementById('duracao').value;
+const duracao = Number(document.getElementById('duracao').value);
+
+let faixa = obterFaixaSelecionada ();
+
+
+
+
+}
